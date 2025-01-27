f92291a4fcdf619e9b8faad5c4810c6bb6c6bcf2
cie10 <leesky0075@dgu.ac.kr>
2025-01-27 16:57:19 +0900

    Init commit

---
+++ src/main/java/com/example/backend_cicd/BackendCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCicdApplication.class, args);
+	}
+
+}
